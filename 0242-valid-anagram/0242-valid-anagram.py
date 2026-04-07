101d728147c83c6dce84608cfe63f235a857e487
Abbas <95271216+AbbasHaq99@users.noreply.github.com>
2026-04-07 12:41:14 -0500

    Time: 15 ms (43.22%), Space: 20.3 MB (11.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        mapS = {}
-        c = 0
-        
-        mapT = {}
-        C = 0
-
-        for i in s:
-            if i.isalpha:
-                mapS[i] = c
-                c + 1
-        
-        for i in reversed(t):
-            if i.isalpha:
-                mapT[i] = C
-                C + 1
-
-        if mapS == mapT:
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
